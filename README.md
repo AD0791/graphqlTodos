467b681eb8a23ae84711753874b0360396d0b479
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2023-04-17 10:20:36 -0400

    basic steps to take to get started gql

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Build a grapqhQL server with mongoldb
 2. activate your virtual environment
 3. run `pip install -r requirements.txt`
 4. run `docker-compose up --build`
+
+## some of queries and mutations
+
+```gql
+mutation createTodoMutation($title: String!, $description: String!) {
+  createTodo(title: $title, description: $description) {
+    todo {
+      id
+      title
+      description
+      completed
+    }
+  }
+}
+```
+
+```gql
+query AllTodo {
+  todos {
+    title
+    description
+    completed
+  }
+}
+```
--- app/schemas/todo_schema.py
+++ app/schemas/todo_schema.py
@@ -1,18 +1,22 @@
 from graphene import Field, List, ObjectType, Schema, String
+from uuid import uuid4
 
 from ..service import CreateTodoMutation
 from ..model import Todo
 
 
 class Query(ObjectType):
-    todo = Field(lambda: Todo, id=String())
     todos = Field(lambda: List(Todo))
 
-    def resolve_todo(self, root, info, id):
-        return next((todo for todo in todos if todo.id == id), None)
-        return todo
-
     def resolve_todos(self, info):
+        todos = [
+            Todo(id=uuid4(), title="Todo 1",
+                 description="This is todo 1", completed=True),
+            Todo(id=uuid4(), title="Todo 2",
+                 description="This is todo 2", completed=False),
+            Todo(id=uuid4(), title="Todo 3",
+                 description="This is todo 3", completed=True),
+        ]
         return todos
 
 
